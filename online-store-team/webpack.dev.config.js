20cd7464c4b77faec3c680a80b6f783962573fc6
petro1909 <petya-97_97@mail.ru>
2022-12-15 19:45:15 +0300

    feat: add historyApiFallback property to devServer object

--- online-store-team/webpack.dev.config.js
+++ online-store-team/webpack.dev.config.js
@@ -4,6 +4,7 @@ module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
+      historyApiFallback: true,
       static: {
         directory:path.resolve(__dirname, './dist'),
       }
