4c7bd84e630797bf065c36429f4861ec10c3d217
petro1909 <petya-97_97@mail.ru>
2022-12-16 19:32:50 +0300

    feat: upgrade router class

--- online-store-team/src/components/router/router.ts
+++ online-store-team/src/components/router/router.ts
@@ -1,25 +1,44 @@
+import ControllerFactory from "../controller/ControllerFactory";
+
 export default class Router {
-    constructor() {
+    public static routes = {
+        404: "error",
+        "/": "store",
+        "/order": "order",
+        "/cart": "cart",
+        "/product": "product",
+    };
+    public start(): void {
         window.onpopstate = this.handleLocation;
-        window.route = this.route;
         this.handleLocation();
     }
-    public readonly routes = {
-        404: "./components/view/404.html",
-        "/": "./components/view/index.html",
-        "/about": "./components/view/about.html",
-        "/lorem": "./components/view/lorem.html",
-    };
-    public route(event: Event) {
+
+    public route = (event: Event) => {
         event = event || window.event;
         event.preventDefault();
-        window.history.pushState({}, "", event.target.href);
-        handleLocation();
-    }
-    public handleLocation = async () => {
-        const path = window.location.pathname;
-        const route = routes[path] || routes[404];
-        const html = await fetch(route).then((data) => data.text());
-        document.getElementById("main-page").innerHTML = html;
+        const { pathname: path } = new URL((event!.target! as HTMLAnchorElement).href);
+        window.history.pushState({ path }, path, path);
+        this.handleLocation();
     };
+
+    private handleLocation = async () => {
+        const fullPath: [string, string] = this.getWindowPathString();
+        const route = Router.routes[fullPath[0] as keyof typeof Router.routes] || Router.routes[404];
+        const controller = ControllerFactory.initController(route);
+        controller.init(fullPath[1]);
+    };
+
+    private getWindowPathString(): [string, string] {
+        let path = window.location.pathname;
+        let endpoint = "";
+        const indexOfSecondSlash = path.indexOf("/", 1);
+        if (indexOfSecondSlash !== -1) {
+            endpoint = path.substring(indexOfSecondSlash + 1);
+            path = path.substring(0, indexOfSecondSlash);
+        }
+        const queryParams = `${endpoint}${window.location.search}`;
+        console.log(path);
+        console.log(queryParams);
+        return [path, queryParams];
+    }
 }
