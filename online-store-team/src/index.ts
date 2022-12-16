de69aa665034097c67f877e70766502f0a5d510b
petro1909 <petya-97_97@mail.ru>
2022-12-16 19:00:54 +0300

    feat: import app start method

--- online-store-team/src/index.ts
+++ online-store-team/src/index.ts
@@ -1,27 +1,4 @@
-const route = (event) => {
-    console.log('dwd')
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handleLocation();
-};
-console.log('dwd')
-const routes = {
-    404: "./components/view/404.html",
-    "/": "./components/view/index.html",
-    "/about": "./components/view/about.html",
-    "/lorem": "./components/view/lorem.html",
-};
+import App from "./components/app/App";
 
-const handleLocation = async () => {
-    console.log('dwd')
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = html;
-};
-
-window.onpopstate = handleLocation;
-window.route = route;
-
-handleLocation();
+const app: App = new App();
+app.start();
