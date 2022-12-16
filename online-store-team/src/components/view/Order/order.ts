6c9d2b784322a395f042fef9d45bd53a0f6a545d
petro1909 <petya-97_97@mail.ru>
2022-12-16 19:46:39 +0300

    feat: initialize order templates

---
+++ online-store-team/src/components/view/Order/order.ts
@@ -0,0 +1,8 @@
+import orderHtml from "./order.html";
+import "./order.css";
+
+export default class OrderView {
+    public drawOrder(): void {
+        document.getElementById("root")!.innerHTML = orderHtml;
+    }
+}
