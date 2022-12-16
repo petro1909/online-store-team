46dcffa9ef1627eb9bd7f29a8ac3ce2b2ff3e444
petro1909 <petya-97_97@mail.ru>
2022-12-16 19:39:44 +0300

    feat: create cart controller class

---
+++ online-store-team/src/components/controller/CartController.ts
@@ -0,0 +1,13 @@
+import CartView from "../view/Cart/cart";
+import BaseController from "./BaseController";
+
+export default class CartController extends BaseController {
+    private cartView: CartView;
+    constructor() {
+        super();
+        this.cartView = new CartView();
+    }
+    public override init(): void {
+        this.cartView.drawCart();
+    }
+}
