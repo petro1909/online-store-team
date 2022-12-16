3bf6253bae1cba2ad2d958007ff4b6bffbad0c6a
petro1909 <petya-97_97@mail.ru>
2022-12-16 19:27:48 +0300

    feat: add start method to App class

--- online-store-team/src/components/app/App.ts
+++ online-store-team/src/components/app/App.ts
@@ -1,7 +1,18 @@
 import Router from "../router/router";
-class App {
-    private router: Router;
+import HeaderView from "../view/Header/header";
+
+export default class App {
+    private readonly header: HeaderView;
+    private readonly router: Router;
     constructor() {
+        this.header = new HeaderView();
         this.router = new Router();
+    }
+
+    public start() {
+        this.router.start();
+        this.header.drawHeader();
+
+        document.querySelectorAll('[href^="/"]').forEach((a) => a.addEventListener("click", this.router.route));
     }
 }
