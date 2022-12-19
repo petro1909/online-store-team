fcfd1ef19ce96a8986dff11d244d853a9f5f034d
petro1909 <petya-97_97@mail.ru>
2022-12-19 13:08:01 +0300

    feat: update app start method

--- online-store-team/src/components/app/App.ts
+++ online-store-team/src/components/app/App.ts
@@ -1,9 +1,14 @@
+import Cart from "../model/Cart";
+import { Store } from "../model/Store";
 import Router from "../router/router";
 import HeaderView from "../view/Header/header";
 
 export default class App {
     private readonly header: HeaderView;
+    private readonly footer: FooterView;
     private readonly router: Router;
+    public static readonly store: Store;
+    public static readonly cart: Cart;
     constructor() {
         this.header = new HeaderView();
         this.router = new Router();
@@ -12,6 +17,7 @@ export default class App {
     public start() {
         this.router.start();
         this.header.drawHeader();
+        this.footer.drawFooter();
 
         document.querySelectorAll('[href^="/"]').forEach((a) => a.addEventListener("click", this.router.route));
     }
