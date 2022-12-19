85af72926df65a294d4b6e74cb1126e9080f6934
petro1909 <petya-97_97@mail.ru>
2022-12-19 13:16:23 +0300

    feat: update init method

--- online-store-team/src/components/controller/ProductController.ts
+++ online-store-team/src/components/controller/ProductController.ts
@@ -1,13 +1,24 @@
+import { Store } from "../model/Store";
 import ProductView from "../view/Product/product";
 import BaseController from "./BaseController";
+import App from "../app/App";
 
 export default class ProductController extends BaseController {
     private productView: ProductView;
+    private store: Store;
     constructor() {
         super();
         this.productView = new ProductView();
+        this.store = App.store;
     }
-    public override init(): void {
-        this.productView.drawProduct();
+    public override async init(options?: string): Promise<void> {
+        let productId: number;
+        if (options) {
+            productId = this.getProductId(options);
+        }
+        let product = this.store.products.find((item) => item.id === productId);
+        this.productView.drawProduct(product);
+    }
+    private getProductId(options: string): number {
     }
 }
