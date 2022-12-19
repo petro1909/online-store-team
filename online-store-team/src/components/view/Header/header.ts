566c55eb2441bf0f9b9e974624e8693dc001aa57
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2022-12-19 22:11:55 +0300

    feat: supplement drawHeader() method in header.ts with the output of cart data

--- online-store-team/src/components/view/Header/header.ts
+++ online-store-team/src/components/view/Header/header.ts
@@ -1,9 +1,11 @@
 import "./header.css";
 import HeaderHtml from "./header.html";
-import Cart from "../../model/Cart"; 
+import Cart from "../../model/Cart";
 
 export default class HeaderView {
     public drawHeader(cart: Cart): void {
         document.getElementById("header")!.innerHTML = HeaderHtml;
+        document.querySelector(".cart-total__cost")!.innerHTML  = String(cart.totalPrice);
+        document.querySelector(".cart-content__quantity")!.innerHTML  = String(cart.cartProducts.length);
     }
 }
