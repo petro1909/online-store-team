46252ff0a23feee371f3c1779177b9dc697e3aff
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2022-12-19 20:54:39 +0300

    feat: add footer.html, footer.css add footer.ts & implement it to app.ts

--- online-store-team/src/components/app/App.ts
+++ online-store-team/src/components/app/App.ts
@@ -2,6 +2,7 @@ import Cart from "../model/Cart";
 import { Store } from "../model/Store";
 import Router from "../router/router";
 import HeaderView from "../view/Header/header";
+import FooterView from "../view/Footer/footer";
 
 export default class App {
     private readonly header: HeaderView;
@@ -10,6 +11,7 @@ export default class App {
     public static readonly store: Store;
     public static readonly cart: Cart;
     constructor() {
+        this.footer = new FooterView();
         this.header = new HeaderView();
         this.router = new Router();
     }
