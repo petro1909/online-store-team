487be87f3283c84f9a073fec0e6e3924b960130f
petro1909 <petya-97_97@mail.ru>
2022-12-19 13:20:05 +0300

    feat: add cart methods

--- online-store-team/src/components/model/Cart.ts
+++ online-store-team/src/components/model/Cart.ts
@@ -1,1 +1,42 @@
-class Cart {}
+export default class Cart {
+    public totalPrice: number;
+    private cartProducts: Array<CartProduct>;
+    public promocode: IPromocode | undefined;
+
+    constructor() {
+        this.totalPrice = 0;
+        this.cartProducts = [];
+    }
+
+    public putProductIntoCart(product: Product): void {
+        const cartProduct: CartProduct = { product: product, count: 1, totalPrice: product.price };
+        this.cartProducts.push(cartProduct);
+        this.totalPrice += product.price;
+    }
+
+    public increaceCartProductCount(cartProductId: number): void {
+        const cartProduct = this.cartProducts.find((item) => item.product.id === cartProductId);
+        if (cartProduct && cartProduct.count < cartProduct.product.stock) {
+            cartProduct.count += 1;
+            cartProduct.totalPrice += cartProduct.product.price;
+            this.totalPrice += cartProduct.product.price;
+        }
+    }
+
+    public decreaceCartProductCount(cartProductId: number): void {
+        const cartProduct = this.cartProducts.find((item) => item.product.id === cartProductId);
+        if (cartProduct) {
+            cartProduct.count -= 1;
+            cartProduct.totalPrice -= cartProduct.product.price;
+            this.totalPrice -= cartProduct.product.price;
+            if (cartProduct.count === 0) {
+                const cartProductIndex = this.cartProducts.indexOf(cartProduct);
+                this.cartProducts.splice(cartProductIndex, 1);
+            }
+        }
+    }
+
+    public setPromocode(promocode: IPromocode) {
+        this.totalPrice = this.totalPrice * (1 - promocode.discount / 100);
+    }
+}
