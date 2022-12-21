fc98af37def96a8287b157b7982ca7956a8ebad3
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2022-12-21 05:27:13 +0300

    feat: implement drawOrder() method in order.ts

--- online-store-team/src/components/view/Order/order.ts
+++ online-store-team/src/components/view/Order/order.ts
@@ -4,6 +4,12 @@ import "./order.css";
 export default class OrderView {
     public drawOrder(): void {
         document.getElementById("root")!.innerHTML = orderHtml;
-        document.
+        document.getElementById("pop-up")!.classList.remove("pop-up_invisible");
+        const order = document.getElementById("order");
+        order!.addEventListener("submit", event => {
+            event.preventDefault();
+            document.getElementById("pop-up")!.classList.add("pop-up_invisible");
+            localStorage.setItem("cart", '');
+        })
     }
 }
