e3a73c990b46db88ca9049d3c9dba4920dba493d
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2022-12-21 05:55:09 +0300

    feat: implement close pop-up in order.ts

--- online-store-team/src/components/view/Order/order.ts
+++ online-store-team/src/components/view/Order/order.ts
@@ -5,11 +5,20 @@ export default class OrderView {
     public drawOrder(): void {
         document.getElementById("root")!.innerHTML = orderHtml;
         document.getElementById("pop-up")!.classList.remove("pop-up_invisible");
-        const order = document.getElementById("order");
+        const order = document.getElementById("order") as HTMLFormElement;
+        const popUp = document.getElementById("pop-up");
         order!.addEventListener("submit", event => {
             event.preventDefault();
             document.getElementById("pop-up")!.classList.add("pop-up_invisible");
+            order!.reset();
             localStorage.setItem("cart", '');
+        })
+        popUp!.addEventListener("click", (event) => {
+            const clickedElement = event.target! as HTMLDivElement;
+            if(clickedElement.classList.contains("pop-up")) {
+                order!.reset();
+                document.getElementById("pop-up")!.classList.add("pop-up_invisible");
+            }
         })
     }
 }
