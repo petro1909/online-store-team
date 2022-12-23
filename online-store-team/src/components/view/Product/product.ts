1ef54ca1498d72083e8825d7e1e60af43c36b201
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2022-12-23 04:07:38 +0300

    fix: fix product view (src/components/view/Product) according with content of the cart

--- online-store-team/src/components/view/Product/product.ts
+++ online-store-team/src/components/view/Product/product.ts
@@ -3,6 +3,7 @@ import productHtml from "./product.html";
 import "./product.css";
 import Cart from "../../model/Cart";
 import { Product } from "../../model/type/IProduct";
+import { app } from "../../..";
 
 export default class ProductView {
     public drawProduct(product: Product, cart: Cart): void {
@@ -10,22 +11,18 @@ export default class ProductView {
         document.getElementById("root")!.innerHTML = productHtml;
 
         const productId = document.getElementById("product-id")!;
-
         const breadcrumbLinkStore = document.getElementById("breadcrumb-link-store")!;
         const breadcrumbLinkCategory = document.getElementById("breadcrumb-link-category")!;
         const breadcrumbLinkBrand = document.getElementById("breadcrumb-link-brand")!;
         const breadcrumbLinkProduct = document.getElementById("breadcrumb-link-product")!;
         const productTitle = document.getElementById("product-title")!;
         const productImage = document.getElementById("product-image")! as HTMLImageElement;
-        const productImagesButtons = document.getElementById("product-images-buttons")!;
-
         const productDescription = document.getElementById("product-description")!;
         const productDiscount = document.getElementById("product-discount")!;
         const productRating = document.getElementById("product-rating")!;
         const productStock = document.getElementById("product-stock")!;
         const productBrand = document.getElementById("product-brand")!;
         const productCategory = document.getElementById("product-category")!;
-
         const cartCost = document.getElementById("cart-cost")!;
 
         productId.setAttribute("data-id", String(product.id));
@@ -35,29 +32,70 @@ export default class ProductView {
         breadcrumbLinkProduct.textContent = product.title;
         productTitle.textContent = product.title;
         productImage.src = `${product.images[0]}`;
-
-        const controls = document.getElementById("product-images-buttons");
-
-        for (let i = 0; i < product.images.length; i++) {
-            const element = document.createElement("button");
-            element.classList.add("controls__btn");
-            element.setAttribute("data-id", String(i));
-            controls!.append(element)
-        }
-
-        productImagesButtons.addEventListener("click", (event) => {
-            const clickedElement = event.target! as HTMLButtonElement;
-            const idx = Number(clickedElement.getAttribute("data-id"))!;
-            productImage.src = `${product.images[idx]}`;
-        });
-
         productDescription.textContent = product.description;
         productDiscount.textContent = product.discountPercentage + "%";
         productRating.textContent = String(product.rating);
         productStock.textContent = String(product.stock);
         productBrand.textContent = product.brand;
         productCategory.textContent = product.category;
+        cartCost.textContent = "€" + cart.totalPrice;
 
-        cartCost.textContent = "€" + cart.totalPrice; // TODO fix to wright value
+        const cartItemsIds  = this.selectCartItemsIds(app.cart);
+        const isId = cartItemsIds.find((id): boolean => { return id === product.id});
+        if(isId) {
+            const currentAddButton = document.querySelector(".object__add-to-cart")! as HTMLElement;
+            ProductView.switchProductButton("DROP FROM CART", currentAddButton);
+        }
+
+        const controlsOfImages = document.getElementById("product-images-buttons")!;
+        for (let i = 0; i < product.images.length; i++) {
+            const element = document.createElement("button");
+            element.classList.add("controls__btn");
+            element.setAttribute("data-id", String(i));
+            controlsOfImages!.append(element)
+        }
+
+        controlsOfImages.addEventListener("click", (event) => {
+            const clickedElement = event.target! as HTMLButtonElement;
+            const idx = Number(clickedElement.getAttribute("data-id"))!;
+            productImage.src = `${product.images[idx]}`;
+        });
+
+        document.querySelector(".object__buttons")!.addEventListener("click", this.productClickHandler);
+
     }
+
+    private productClickHandler(event: Event): void {
+
+        const clickedElement = event.target as HTMLElement;
+        const cartCost = document.getElementById("cart-cost")!;
+        const productId = clickedElement.closest(".product__obj")!.getAttribute("data-id")!;
+        const product = app.store.products.find((item) => item.id === +productId!);
+
+        if (clickedElement.classList.contains("object__add-to-cart")) {
+            app.cart.putProductIntoCart(product!);
+            app.header.drawHeader(app.cart);
+            cartCost.textContent = "€" + app.cart.totalPrice;
+            ProductView.switchProductButton("DROP FROM CART", clickedElement);
+        } else if (clickedElement.classList.contains("object__drop-from-cart")) {
+            app.cart.dropProductIntoCart(Number(productId));
+            app.header.drawHeader(app.cart);
+            cartCost.textContent = "€" + app.cart.totalPrice;
+            ProductView.switchProductButton("ADD TO CART", clickedElement);
+        }
+
+    }
+
+    private selectCartItemsIds(cart: Cart) {
+        const items = cart.cartProducts;
+        const itemIds: Array<number> = items.map((item): number => { return item.product.id})
+        return itemIds;
+    }
+
+    private static switchProductButton(textContent: string, clickedButton: HTMLElement): void {
+        clickedButton.classList.toggle("object__drop-from-cart");
+        clickedButton.classList.toggle("object__add-to-cart");
+        clickedButton.textContent = textContent;
+    }
+
 }
