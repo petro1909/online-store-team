a835a0c736d9f480b84a094e9f2d7d0b2bb0fa2b
petro1909 <petya-97_97@mail.ru>
2022-12-26 23:34:29 +0300

    feat: add CartOptions class constructor with default property values

--- online-store-team/src/components/model/type/IFilterOptions.ts
+++ online-store-team/src/components/model/type/IFilterOptions.ts
@@ -13,6 +13,10 @@ export class StoreFilterOptions {
 export class CartOptions {
     page = 0;
     limit = 0;
+    constructor(page = 1, limit = 3) {
+        this.page = page;
+        this.limit = limit;
+    }
 }
 // export interface ISearchOptions extends IBaseOptions {
 //     searchString: string;
