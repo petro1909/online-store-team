c05bf0fd06b4160a0f0ec365e794fa0ade13204e
petro1909 <petya-97_97@mail.ru>
2022-12-26 22:40:31 +0300

    fix: change StoreFilterOptions and CartOptions to class

--- online-store-team/src/components/model/type/IFilterOptions.ts
+++ online-store-team/src/components/model/type/IFilterOptions.ts
@@ -1,31 +1,19 @@
-export class IStoreFilterOptions {
+export class StoreFilterOptions {
     categories: Array<string> = [];
     brands: Array<string> = [];
-    minPrice!: number;
-    maxPrice?: number;
-    minStock?: number;
-    maxStock?: number;
-    searchString?: string;
-    displayMode?: string;
-    sortingString?: string;
-    constructor() {
-        this.categories = [];
-        this.brands = [];
-        this.minPrice = 0;
-        this.maxPrice = 0;
-        this.minStock = 0;
-        this.maxStock = 0;
-        this.searchString = "";
-        this.displayMode = "";
-        this.sortingString = "";
-    }
+    minPrice = 0;
+    maxPrice = 0;
+    minStock = 0;
+    maxStock = 0;
+    searchString = "";
+    displayMode = "";
+    sortingString = "";
 }
 
-export type ICartOptions = {
-    page?: number;
-    limit?: number;
-};
-
+export class CartOptions {
+    page = 0;
+    limit = 0;
+}
 // export interface ISearchOptions extends IBaseOptions {
 //     searchString: string;
 // }
@@ -38,7 +26,3 @@ export interface ISortOptions {
     sortingParameter: string;
     order: number;
 }
-
-// export interface IBaseOptions {
-//     [key: string]: string | number | Array<string> | undefined;
-// }
