ae9e2ec892a40b3f854e21a18b8062e072ef32eb
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2023-01-08 19:39:25 +0300

    feat: make test "test/components/controller/cartController.test.ts"

--- online-store-team/test/components/controller/cartController.test.ts
+++ online-store-team/test/components/controller/cartController.test.ts
@@ -1,8 +1,12 @@
 import CartController from "../../../src/components/controller/cartController";
 
 const cartController = new CartController();
+const testQueryString = "?page=1&limit=3"
+
 describe("test cart controller getCartOptions method", () => {
     it("returns right cart options after parsing query string", () => {
-        //
+        const expected = {page: 1, limit: 3};
+        const result = cartController.getCartOptions(testQueryString);
+        expect(result).toEqual(expected);
     });
 });
