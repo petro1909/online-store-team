66e72a52ffd708682209caae7826e41b58ca4f8d
petro1909 <petya-97_97@mail.ru>
2023-01-08 20:47:26 +0300

    refactor: delete unused files

--- online-store-team/src/components/view/Error/error.ts
+++
@@ -1,8 +0,0 @@
-import errorHtml from "./error.html";
-import "./error.css";
-
-export default class ErrorView {
-    public drawError(): void {
-        document.getElementById("root")!.innerHTML = errorHtml;
-    }
-}
