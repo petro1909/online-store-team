80e71d691fa763b41b53cee615fd89d750521392
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2023-01-08 21:38:11 +0300

    feat: make test for "parseQueryParameters()" method in "test/components/router/router.test.ts"

--- online-store-team/test/components/router/router.test.ts
+++ online-store-team/test/components/router/router.test.ts
@@ -1,8 +1,51 @@
-// import Router from "../../../src/components/router/router";
+import Router from "../../../src/components/router/router";
 
-// const testRouter = new Router();
-//нужно замокать window.loaction, чтобы можно было использовать
-//window.location.pathname и window.location.search
+const testRouter = new Router();
+
+// const stubLocalCartWindowLocation = {
+//     "href": "http://localhost:8080/cart?page=1&limit=3",
+//     "pathname": "/cart",
+//     "search": "?page=1&limit=3",
+// }
+
+// const stubDeployCartWindowLocation = {
+//     "href": "https://petro1909-online-store-team.netlify.app/cart?page=1&limit=3",
+//     "pathname": "/cart",
+//     "search": "?page=1&limit=3",
+// }
+
+// const stubLocalStoreWindowLocation = {
+//     "href": "http://localhost:8080/?categories=smartphones&brands=Apple&minPrice=10&maxPrice=1749",
+//     "pathname": "/",
+//     "search": "?categories=smartphones&brands=Apple&minPrice=10&maxPrice=1749",
+// }
+
+// const stubDeployStoreWindowLocation = {
+//     "href": "https://petro1909-online-store-team.netlify.app/?categories=smartphones&brands=Apple&minPrice=10&maxPrice=1749",
+//     "pathname": "/",
+//     "search": "?categories=smartphones&brands=Apple&minPrice=10&maxPrice=1749",
+// }
+
+const stubStoreFilterOptions = {
+    "categories": [
+        "smartphones"
+    ],
+    "brands": [
+        "Apple"
+    ],
+    "minPrice": 0,
+    "maxPrice": 0,
+    "minStock": 0,
+    "maxStock": 0,
+    "searchString": "",
+    "displayMode": "",
+    "sortingString": ""
+}
+
+const stubCartOptions = {
+    "page": 1,
+    "limit": 3
+}
 
 describe("test router model functions", () => {
     describe("test getWindowPathString method", () => {
@@ -12,10 +55,14 @@ describe("test router model functions", () => {
     });
     describe("test parseQueryParameters method", () => {
         it("returns right path string depends on StoreFilterOptions", () => {
-            //
+            const expected = "/?categories=smartphones&brands=Apple";
+            const result = testRouter.parseQueryParameters(stubStoreFilterOptions);
+            expect(result).toEqual(expected);
         });
         it("returns right path string depends on CartOptions", () => {
-            //
+            const expected = "/cart?page=1&limit=3";
+            const result = testRouter.parseQueryParameters(stubCartOptions);
+            expect(result).toEqual(expected);
         });
     });
 });
