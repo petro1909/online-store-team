0f806b755def4a1c0a3075af72903faafc89d592
petro1909 <petya-97_97@mail.ru>
2023-01-08 20:42:33 +0300

    refactor: change file and folder name cases

---
+++ online-store-team/src/components/view/header/header.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import "./header.css";
+import HeaderHtml from "./header.html";
+import Cart from "../../model/cartModel";
+
+export default class HeaderView {
+    public drawHeader(cart: Cart): void {
+        document.getElementById("header")!.innerHTML = HeaderHtml;
+        document.querySelector(".cart-total__cost")!.innerHTML = String(cart.totalPrice);
+        document.querySelector(".cart-content__quantity")!.innerHTML = String(cart.totalCount);
+    }
+}
