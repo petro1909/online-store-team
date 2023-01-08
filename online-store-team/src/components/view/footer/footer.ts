14a50475732c9bef22a305fb569f38d70ad61122
petro1909 <petya-97_97@mail.ru>
2023-01-08 21:56:32 +0300

    refactor: rename folder

--- online-store-team/src/components/view/footer/footer.ts
+++
@@ -1,8 +0,0 @@
-import "./footer.css";
-import FooterHtml from "./footer.html";
-
-export default class FooterView {
-    public drawFooter(): void {
-        document.getElementById("footer")!.innerHTML = FooterHtml;
-    }
-}
