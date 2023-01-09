69c3c609f6e4815c47f730f844715c36312a557a
petro1909 <petya-97_97@mail.ru>
2023-01-09 11:23:04 +0300

    refactor: refactor store filter methods

--- online-store-team/src/components/model/storeFilterModel.ts
+++ online-store-team/src/components/model/storeFilterModel.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { StoreFilterOptions } from "./storeOptions";
 import { ISortOptions } from "./type/sortOptions";
 import { Product } from "./type/product";
@@ -30,143 +29,172 @@ export default class StoreFilter {
     public maxStock = 0;
 
     constructor(products: Array<Product>) {
-        for (const product of products) {
-            this.initFilterFields(product);
-        }
+        this.initFilterFields(products);
     }
-    // public initFilterFields(products: Array<Product>) {
-    //     const productPriceArray = products.map((product) => product.price);
-    //     this.minPrice = Math.min(...productPriceArray);
-    //     this.maxPrice = Math.max(...productPriceArray);
-    //     const productStockArray = products.map((product) => product.stock);
-    //     this.minPrice = Math.min(...productStockArray);
-    //     this.maxPrice = Math.max(...productStockArray);
-    //     products.forEach((product) => {
-    //         //fill products count by category
-    //         let categoryProduct = this.categoryProducts.find((item) => item.category === product.category);
-    //         if (!categoryProduct) {
-    //             categoryProduct = { category: product.category, activeProducts: 1, totalProducts: 1 };
-    //             this.categoryProducts.push(categoryProduct);
-    //         } else {
-    //             categoryProduct.totalProducts += 1;
-    //             categoryProduct.activeProducts += 1;
-    //         }
-    //         //fill products count by brand
-    //         let brandProduct = this.brandProducts.find((item) => item.brand === product.brand);
-    //         if (!brandProduct) {
-    //             brandProduct = { brand: product.brand, activeProducts: 1, totalProducts: 1 };
-    //             this.brandProducts.push(brandProduct);
-    //         } else {
-    //             brandProduct.totalProducts += 1;
-    //             brandProduct.activeProducts += 1;
-    //         }
-    //     });
-    // }
+    public initFilterFields(products: Array<Product>) {
+        const productPriceArray = products.map((product) => product.price);
+        this.minPrice = Math.min(...productPriceArray);
+        this.maxPrice = Math.max(...productPriceArray);
+        const productStockArray = products.map((product) => product.stock);
+        this.minStock = Math.min(...productStockArray);
+        this.maxStock = Math.max(...productStockArray);
 
-    public initFilterFields(product: Product): void {
-        //fill products count by category
-        let categoryProduct = this.categoryProducts.find((item) => item.category === product.category);
-        if (!categoryProduct) {
-            categoryProduct = { category: product.category, activeProducts: 1, totalProducts: 1 };
-            this.categoryProducts.push(categoryProduct);
-        } else {
-            categoryProduct.totalProducts += 1;
-            categoryProduct.activeProducts += 1;
-        }
-        //fill products count by brand
-        let brandProduct = this.brandProducts.find((item) => item.brand === product.brand);
-        if (!brandProduct) {
-            brandProduct = { brand: product.brand, activeProducts: 1, totalProducts: 1 };
-            this.brandProducts.push(brandProduct);
-        } else {
-            brandProduct.totalProducts += 1;
-            brandProduct.activeProducts += 1;
-        }
-        //fill minPrice
-        if (product.price < this.minPrice) {
-            this.minPrice = product.price;
-        }
-        if (product.price > this.maxPrice) {
-            this.maxPrice = product.price;
-        }
-        if (product.stock < this.minStock) {
-            this.minStock = product.stock;
-        }
-        if (product.stock > this.maxStock) {
-            this.maxStock = product.stock;
-        }
+        this.categoryProducts = Array.from(new Set(products.map((product) => product.category))).map(
+            (uniqeCategory) => {
+                const categoryProduct = { category: uniqeCategory, activeProducts: 0, totalProducts: 0 };
+                return categoryProduct;
+            }
+        );
+        this.categoryProducts.forEach((productByCategory) => {
+            const categoryProduct = products.filter((product) => product.category === productByCategory.category);
+            productByCategory.totalProducts = productByCategory.activeProducts = categoryProduct.length;
+        });
+        this.brandProducts = Array.from(new Set(products.map((product) => product.brand))).map((uniqeBrand) => {
+            const brandProduct = { brand: uniqeBrand, activeProducts: 0, totalProducts: 0 };
+            return brandProduct;
+        });
+        this.brandProducts.forEach((productByBrand) => {
+            const brandProduct = products.filter((product) => product.brand === productByBrand.brand);
+            productByBrand.totalProducts = productByBrand.activeProducts = brandProduct.length;
+        });
     }
 
-    public updateFilter(products: Array<Product>, options: StoreFilterOptions): Array<Product> {
-        const activeProducts: Array<Product> = [];
-        for (const product of products) {
-            if (this.filterProduct(product, options) && this.searchProduct(product, options)) {
-                activeProducts.push(product);
-            }
-        }
-        if (activeProducts[0] !== undefined) {
-            this.minPrice = activeProducts[0].price;
-            this.maxPrice = activeProducts[0].price;
-            this.minStock = activeProducts[0].stock;
-            this.maxStock = activeProducts[0].stock;
-        }
-        this.resetBrandAndCaterory();
-        for (const product of activeProducts) {
-            this.updateFilterFields(product, options);
-        }
-        return this.sortProducts(activeProducts, options);
+    public getActiveProducts(products: Array<Product>, options: StoreFilterOptions): Array<Product> {
+        let activeProducts = this.filterProducts(products, options);
+        console.log(activeProducts);
+        activeProducts = this.searchProducts(activeProducts, options);
+        activeProducts = this.sortProducts(activeProducts, options);
+        return activeProducts;
     }
-    private resetBrandAndCaterory() {
-        this.categoryProducts.map((item) => (item.activeProducts = 0));
-        this.brandProducts.map((item) => (item.activeProducts = 0));
+    public updateFilterFields(products: Array<Product>, options?: StoreFilterOptions) {
+        if (!options) {
+            return;
+        }
+        const productPriceArray = products.map((product) => product.price);
+        if (options.minPrice > 0) {
+            this.minPrice = options.minPrice;
+        } else {
+            this.minPrice = Math.min(...productPriceArray);
+        }
+        if (options.maxPrice > 0) {
+            this.maxPrice = options.maxPrice;
+        } else {
+            this.maxPrice = Math.max(...productPriceArray);
+        }
+        const productStockArray = products.map((product) => product.stock);
+        if (options.minStock > 0) {
+            this.minStock = options.minStock;
+        } else {
+            this.minStock = Math.min(...productStockArray);
+        }
+        if (options.maxStock > 0) {
+            this.maxStock = options.maxStock;
+        } else {
+            this.maxStock = Math.max(...productStockArray);
+        }
+
+        this.categoryProducts.forEach((productByCategory) => {
+            productByCategory.activeProducts = 0;
+            const categoryProduct = products.filter((product) => product.category === productByCategory.category);
+            productByCategory.activeProducts = categoryProduct.length;
+        });
+        this.brandProducts.forEach((productByBrand) => {
+            productByBrand.activeProducts = 0;
+            const brandProduct = products.filter((product) => product.brand === productByBrand.brand);
+            productByBrand.activeProducts = brandProduct.length;
+        });
     }
-    private updateFilterFields(product: Product, options: StoreFilterOptions) {
-        //fill products count by category
-        const categoryProduct = this.categoryProducts.find((item) => item.category === product.category);
-        if (categoryProduct) {
-            categoryProduct.activeProducts += 1;
-        }
-        //fill products count by brand
-        const brandProduct = this.brandProducts.find((item) => item.brand === product.brand);
-        if (brandProduct) {
-            brandProduct.activeProducts += 1;
-        }
-        //fill minPrice
-        const minPrice = options.minPrice;
-        if (minPrice) {
-            this.minPrice = minPrice;
-        } else {
-            if (product.price < this.minPrice) {
-                this.minPrice = product.price;
+
+    private filterProducts(products: Array<Product>, filterOptions: StoreFilterOptions): Array<Product> {
+        const activeProducts = products.filter((product) => {
+            const categories = filterOptions.categories;
+            if (categories.length !== 0) {
+                const isInCategory = categories.find((category) => category === product.category);
+                if (!isInCategory) return false;
             }
-        }
-        const maxPrice = options.maxPrice;
-        if (maxPrice) {
-            this.maxPrice = maxPrice;
-        } else {
-            if (product.price > this.maxPrice) {
-                this.maxPrice = product.price;
+            const brands = filterOptions.brands;
+            if (brands.length !== 0) {
+                const isInBrand = brands.find((brand) => brand === product.brand);
+                if (!isInBrand) return false;
             }
-        }
-        const minStock = options.minStock;
-        if (minStock) {
-            this.minStock = minStock;
-        } else {
-            if (product.stock < this.minStock) {
-                this.minStock = product.stock;
+            const minPrice = filterOptions.minPrice;
+            if (minPrice) {
+                const isInPrice = product.price >= minPrice;
+                if (!isInPrice) return false;
             }
-        }
-        const maxStock = options.maxStock;
-        if (maxStock) {
-            this.maxStock = maxStock;
-        } else {
-            if (product.stock > this.maxStock) {
-                this.maxStock = product.stock;
+            const maxPrice = filterOptions.maxPrice;
+            if (maxPrice) {
+                const isInPrice = product.price <= maxPrice;
+                if (!isInPrice) return false;
             }
-        }
+            const minStock = filterOptions.minStock;
+            if (minStock) {
+                const isInStock = product.stock >= minStock;
+                if (!isInStock) return false;
+            }
+            const maxStock = filterOptions.maxStock;
+            if (maxStock) {
+                const isInStock = product.stock <= maxStock;
+                if (!isInStock) return false;
+            }
+            return true;
+        });
+        return activeProducts;
     }
-    private filterProduct(item: Product, filterOptions: StoreFilterOptions): boolean {
-        //app.router.addQueryParameters(filterOptions);
+
+    private sortProducts(products: Array<Product>, filterOptions: StoreFilterOptions): Array<Product> {
+        const sortingString = filterOptions.sortingString;
+        if (!sortingString) {
+            return products;
+        }
+        const sortingOptions = StoreFilter.sortMap.get(sortingString);
+        if (!sortingOptions) {
+            return products;
+        }
+        return products.sort((a, b) => {
+            const par1 = a[sortingOptions.sortingParameter as keyof Product];
+            const par2 = b[sortingOptions.sortingParameter as keyof Product];
+            if (sortingOptions.order === 1) {
+                if (par1 > par2) {
+                    return 1;
+                } else if (par1 < par2) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            } else {
+                if (par1 < par2) {
+                    return 1;
+                } else if (par1 > par2) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+    }
+
+    private searchProducts(products: Array<Product>, filterOptions: StoreFilterOptions): Array<Product> {
+        const searchString = filterOptions.searchString;
+        if (!searchString) {
+            return products;
+        }
+        const lowerSearchString = searchString.toLowerCase();
+        return products.filter(
+            (item) =>
+                item.title.toLowerCase().includes(lowerSearchString) ||
+                item.description.toLowerCase().includes(lowerSearchString) ||
+                item.price.toString().includes(lowerSearchString) ||
+                item.discountPercentage.toString().includes(lowerSearchString) ||
+                item.rating.toString().includes(lowerSearchString) ||
+                item.stock.toString().includes(lowerSearchString) ||
+                item.brand.toLowerCase().includes(lowerSearchString) ||
+                item.category.toLowerCase().includes(lowerSearchString) ||
+                item.thumbnail.toLowerCase().includes(lowerSearchString)
+        );
+    }
+
+    public filterProduct(item: Product, filterOptions: StoreFilterOptions): boolean {
         const categories = filterOptions.categories;
         if (categories && categories.length > 0) {
             const isInCategory = categories.find((category) => category.toLowerCase() === item.category.toLowerCase());
@@ -206,7 +234,6 @@ export default class StoreFilter {
             return true;
         }
         const lowerSearchString = searchString.toLowerCase();
-        //app.router.addQueryParameters(searchOptions);
         return (
             item.title.toLowerCase().includes(lowerSearchString) ||
             item.description.toLowerCase().includes(lowerSearchString) ||
@@ -218,91 +245,5 @@ export default class StoreFilter {
             item.category.toLowerCase().includes(lowerSearchString) ||
             item.thumbnail.toLowerCase().includes(lowerSearchString)
         );
-    }
-
-    public getActiveProducts(products: Array<Product>, options: StoreFilterOptions) {
-        let activeProducts = this.filterProducts(products, options);
-        activeProducts = this.searchProducts(products, options);
-        activeProducts = this.sortProducts(products, options);
-        return activeProducts;
-    }
-
-    private filterProducts(products: Array<Product>, filterOptions: StoreFilterOptions): Array<Product> {
-        //app.router.addQueryParameters(filterOptions);
-        return products.filter((item) => {
-            const categories = filterOptions.categories;
-            if (categories) {
-                const isInCategory = categories.find((category) => category === item.category);
-                if (!isInCategory) return false;
-            }
-            const brands = filterOptions.brands;
-            if (brands) {
-                const isInBrand = brands.find((brand) => brand === item.brand);
-                if (!isInBrand) return false;
-            }
-            const minPrice = filterOptions.minPrice;
-            if (minPrice) {
-                const isInPrice = item.price <= minPrice;
-                if (!isInPrice) return false;
-            }
-            const maxPrice = filterOptions.maxPrice;
-            if (maxPrice) {
-                const isInPrice = item.price >= maxPrice;
-                if (!isInPrice) return false;
-            }
-            const minStock = filterOptions.minStock;
-            if (minStock) {
-                const isInStock = item.stock <= minStock;
-                if (!isInStock) return false;
-            }
-            const maxStock = filterOptions.maxStock;
-            if (maxStock) {
-                const isInStock = item.stock >= maxStock;
-                if (!isInStock) return false;
-            }
-            return true;
-        });
-    }
-
-    private sortProducts(products: Array<Product>, filterOptions: StoreFilterOptions): Array<Product> {
-        const sortingString = filterOptions.sortingString;
-        if (!sortingString) {
-            return products;
-        }
-        const sortingOptions = StoreFilter.sortMap.get(sortingString);
-        if (!sortingOptions) {
-            return products;
-        }
-        //app.router.addQueryParameters(sortOptions);
-        return products.sort((a, b) => {
-            const par1 = a[sortingOptions.sortingParameter as keyof Product];
-            const par2 = b[sortingOptions.sortingParameter as keyof Product];
-            if (sortingOptions.order === 1) {
-                if (par1 > par2) {
-                    return 1;
-                } else if (par1 < par2) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            } else {
-                if (par1 < par2) {
-                    return 1;
-                } else if (par1 > par2) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            }
-        });
-    }
-
-    private searchProducts(products: Array<Product>, filterOptions: StoreFilterOptions): Array<Product> {
-        const searchString = filterOptions.searchString;
-        if (!searchString) {
-            return products;
-        }
-        //app.router.addQueryParameters(searchOptions);
-        return products.filter((item) => item.title.includes(searchString) || item.description.includes(searchString));
     }
 }
