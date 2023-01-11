8557c7a9dcde0e93f2bb7037a6ab41e8b8bfc5f2
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2023-01-11 23:54:46 +0300

    fix: fix redirect from "404 page" to main page

--- online-store-team/src/components/view/errorView/error.ts
+++ online-store-team/src/components/view/errorView/error.ts
@@ -1,8 +1,13 @@
 import errorHtml from "./error.html";
+import { app } from "../../../index";
 import "./error.css";
 
 export default class ErrorView {
     public drawError(): void {
         document.getElementById("root")!.innerHTML = errorHtml;
+        const pageButton = document.getElementById("page-button") as HTMLButtonElement;
+        pageButton.onclick = () => {
+            app.router.route("/");
+        }
     }
 }
